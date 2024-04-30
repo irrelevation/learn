2652a4b1c4ac5f63a11637b90c911f293a49b8a5
Lukas Zöllner <lukas.zoellner@teclead-ventures.de>
2024-04-30 10:06:13 +0200

    finish crystal go

--- algorithms-and-datastructures/search/crystal.go
+++ algorithms-and-datastructures/search/crystal.go
@@ -1,20 +1,22 @@
 package search
 
-import "math"
+import (
+	"math"
+)
 
 // Crystal is a function that determines the first position from wich the crystal ball breaks.
 // It takes a slice of boolean values. values are false until they are true. they are not mixed.
 // It returns the index of the first break found, or -1 if no breaks are found.
 func Crystal(breaks []bool) int {
 	step := int(math.Sqrt(float64(len(breaks))))
-	i := 0
+	i := step
 	for ; i < len(breaks); i += step {
 		if breaks[i] {
 			break
 		}
 	}
 
-	for j := i - step; j < i; j++ {
+	for j := i - step; j <= i && j < len(breaks); j++ {
 		if breaks[j] {
 			return j
 		}
--- algorithms-and-datastructures/search/crystal_test.go
+++ algorithms-and-datastructures/search/crystal_test.go
@@ -11,23 +11,48 @@ func TestCrystal(t *testing.T) {
 		expected int
 	}{
 		{
-			name:     "Test case 1",
+			name:     "Test case 1a",
+			breaks:   []bool{false, false, false, true, true, true, true},
+			expected: 3,
+		},
+		{
+			name:     "Test case 1b",
 			breaks:   []bool{false, false, false, false, true, true, true, true},
 			expected: 4,
 		},
 		{
-			name:     "Test case 2",
+			name:     "Test case 1c",
+			breaks:   []bool{false, false, false, false, true, true, true},
+			expected: 4,
+		},
+		{
+			name:     "Test case 2a",
 			breaks:   []bool{false, false, false, false, false, false, false, true},
 			expected: 7,
 		},
 		{
-			name:     "Test case 3",
+			name:     "Test case 2b",
+			breaks:   []bool{false, false, false, false, false, false, true},
+			expected: 6,
+		},
+		{
+			name:     "Test case 4a",
 			breaks:   []bool{true, true, true, true, true, true, true, true},
 			expected: 0,
 		},
 		{
-			name:     "Test case 4",
+			name:     "Test case 4b",
+			breaks:   []bool{true, true, true, true, true, true, true},
+			expected: 0,
+		},
+		{
+			name:     "Test case 5a",
 			breaks:   []bool{false, false, false, false, false, false, false, false},
+			expected: -1,
+		},
+		{
+			name:     "Test case 5b",
+			breaks:   []bool{false, false, false, false, false, false, false},
 			expected: -1,
 		},
 	}
