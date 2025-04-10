1c725217976f002eb0aba46c1ca7332bc20aa0c0
Lukas Zöllner <lukas.zoellner@teclead-ventures.de>
2025-04-10 16:05:51 +0200

    format

--- algorithms/search/binary.go
+++ algorithms/search/binary.go
@@ -7,18 +7,18 @@ func Binary[T constraints.Ordered](slice []T, value T) int {
 	hi := len(slice) - 1
 
 	for {
-		m := lo + (hi - lo) / 2
+		m := lo + (hi-lo)/2
 		v := slice[m]
 
-		if (value == v) {
+		if value == v {
 			return m
 		}
 
 		if lo >= hi {
 			return -1
 		}
-		
-		if (value < v) {
+
+		if value < v {
 			hi = m
 		} else {
 			lo = m + 1
