a6c6a1a89aacfc9f5a059bc8e6c475e5fd8b6e14
Lukas Zöllner <lukas.zoellner@teclead-ventures.de>
2025-04-10 16:06:04 +0200

    remove unfinished node stuff

--- projects/async-request-queue/typescript/src/test.ts
+++
@@ -1,7 +0,0 @@
-import assert from "node:assert";
-import test from "node:test";
-
-test("synchronous passing test", (t) => {
-  // This test passes because it does not throw an exception.
-  assert.strictEqual(1, 1);
-});
